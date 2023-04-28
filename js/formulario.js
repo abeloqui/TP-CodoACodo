81a0c9efebff6656fa954a0939a42d6b37ab0086
A.Beloqui <andrebeloqui@gmail.com>
2023-04-28 16:48:47 -0300

    Primer commit

---
+++ js/formulario.js
@@ -0,0 +1,13 @@
+document.getElementById('donante-formulario').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    var nombre = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
+    var telefono = document.getElementById('telefono').value;
+  
+    // Realizar acciones con los datos ingresados
+  
+    document.getElementById('donante-formulario').reset();
+  
+    alert('¡Gracias por tu interés en ser donante! Nos pondremos en contacto contigo pronto.');
+  });
