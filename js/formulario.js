4823e6ac6efc0a6ca8f8e7ad5d52935b29f38404
VeronicaLossi <veronicalossi@gmail.com>
2023-05-22 17:25:58 -0300

    agregado de animacion en  imágenes de testimonios

--- js/formulario.js
+++ js/formulario.js
@@ -13,3 +13,22 @@ document.getElementById('donante-formulario').addEventListener('submit', functio
   
     alert('¡Gracias por tu interés en ser donante! Nos pondremos en contacto contigo pronto.');
   });
+  
+ <script>
+  const miImagen = document.getElementById("myImage");
+
+  // Función para iniciar la animación
+  function startAnimation() {
+    miImagen.style.animationPlayState = "running";
+  }
+
+  // Función para detener la animación
+  function stopAnimation() {
+    miImagen.style.animationPlayState = "paused";
+  }
+
+  // Eventos del cursor
+  miImagen.addEventListener("mouseover", startAnimation);
+  miImagen.addEventListener("mouseout", stopAnimation);
+</script>
+
